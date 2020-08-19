052da4b3e25e055bc164aa3e678a22a6596c4414
Skyler Olson <skyler.olson.rl@gmail.com>
2020-08-19 12:41:28 -0700

    styling up the main page

--- src/components/Posts/LastContent.tsx
+++ src/components/Posts/LastContent.tsx
@@ -7,14 +7,11 @@ import Img from "gatsby-image"
 
 export default ({
   content,
-  isUICol,
 }: {
   content: IPostsContent[]
-  isUICol: Boolean
 }) => (
   <>
-    {content.map((post: IPostsContent, i: number) => {
-      return isUICol ? (
+    {content.map((post: IPostsContent, i: number) => (
         <Link to={post.slug} className="col-md-6" key={i}>
           <h3>{post.title}</h3>
           <div></div>
@@ -29,25 +26,6 @@ export default ({
             <span className="post-date">{post.date}</span>
           </p>
         </Link>
-      ) : (
-        <Link to={post.slug} className="post-row" key={i}>
-          <Img
-            className="post-image"
-            {...(post.childImageSharp as GatsbyImageProps)}
-          />
-          <div className="post-content">
-            <h3>{post.title}</h3>
-            <p>{post.excerpt}</p>
-
-            <p className="post-bottom">
-              <span className="post-to-read">
-                {post.timeToRead} min to read
-              </span>
-              <span className="post-date">{post.date}</span>
-            </p>
-          </div>
-        </Link>
-      )
-    })}
+      ))}
   </>
 )
